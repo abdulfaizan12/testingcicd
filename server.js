ceb4bd944b7202a1ad8c27d246f80e2b7bc4b50b
abdulfaizan12 <abdul.faizan@qbsco.net>
2025-04-09 19:13:24 +0500

    add testing

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Simple GET endpoint
+app.get('/api/hello', (req, res) => {
+  res.json({
+    message: 'Hello from your GET API!',
+    success: true
+  });
+});
+
+console.log("testing");
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
