8ce319730446a01569f33724a54d2d0a11282a69
abdulfaizan12 <abdul.faizan@qbsco.net>
2025-04-14 11:26:22 +0500

    testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3040;
 // Simple GET endpoint
 app.get('/api/hello', (req, res) => {
   res.json({
-    message: 'Hello from your GET API testing 333!',
+    message: 'Hello from your GET API testing!',
     success: true
   });
 });
